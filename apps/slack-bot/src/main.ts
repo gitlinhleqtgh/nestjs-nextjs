100b26061528005a3a9674d2f2c47e526ebd3087
gitlinhleqtgh <linhleqtgh@gmail.com>
2023-09-21 21:52:06 +0700

    feat: 1 init nextjs-nestjs project

---
+++ apps/slack-bot/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.NEXT_PUBLIC_NESTJS_BOT_SERVER || 4001);
+}
+bootstrap();
